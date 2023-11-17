d6d0a90aa8fde7e33c88df9a3c81681493fa97fa
toriumi070808 <146149004+toriumi070808@users.noreply.github.com>
2023-11-17 13:54:41 +0900

    Add files via upload

---
+++ result.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="mystyle.css">
+ 
+<hgroup>
+<h3>タイピング★スター</h3>
+<h4>詳細</h4>
+<h4>タイプミス</h4>
+<h5>スコア</h5>
+<h6>ランク</h6>
+
+</hgroup>
+<div><input type="submit" onclick="location.href='./typing1.php'" value="もう一回"></div>
+<div2><input type="submit" onclick="location.href='./ranking.php'" value="ランキング"></div2>
+<div3><input type="submit" onclick="location.href='./top.php'" value="Topに戻る"></div3>
+ 
